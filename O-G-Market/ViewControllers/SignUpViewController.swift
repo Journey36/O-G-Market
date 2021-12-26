5c48725aeeb35867b51bb7ffe7069a7a9716487e
Journey36 <be9.smile@gmail.com>
2021-12-26 23:48:23 +0900

    chore: 서버 요구사항에 따른 가입화면을 위한 컨트롤러 추가
    + SignUpViewController

---
+++ O-G-Market/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewController.swift
+//  O-G-Market
+//
+//  Created by 재현 on 2021/12/26.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
